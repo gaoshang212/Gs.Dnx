d2ececfb86143d393486b7f06515c1e8c136be49
高尚 <gaoshang212@hotmail.com>
2016-09-09 11:09:19 +0800

    Add project files.

---
+++ src/Gs.Dnx/IoC.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Gs.Dnx
+{
+    public class IoC
+    {
+        private static IServiceProvider _service;
+
+        public static void SetServiceProvider(IServiceProvider p_service)
+        {
+            if (p_service == null)
+            {
+                throw new ArgumentNullException(nameof(p_service));
+            }
+
+            _service = p_service;
+        }
+
+        public static T Get<T>()
+        {
+            return (T)GetService(typeof(T));
+        }
+
+        public static object GetService(Type p_type)
+        {
+            return _service.GetService(p_type);
+        }
+    }
+}
---
+++ src/Gs.Dnx/RouteConstraint/DateTimeExRouteConstraint.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Globalization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+
+namespace Gs.Dnx.RouteConstraint
+{
+    public class DateTimeExRouteConstraint : IRouteConstraint
+    {
+        private readonly string _dateTimeformat;
+
+        public DateTimeExRouteConstraint(string p_dateTimeformat)
+        {
+            _dateTimeformat = p_dateTimeformat;
+        }
+
+        public DateTimeExRouteConstraint()
+        {
+
+        }
+
+        public bool Match(HttpContext httpContext, IRouter route, string routeKey, RouteValueDictionary values, RouteDirection routeDirection)
+        {
+            if (httpContext == null)
+            {
+                throw new ArgumentNullException(nameof(httpContext));
+            }
+
+            if (route == null)
+            {
+                throw new ArgumentNullException(nameof(route));
+            }
+
+            if (routeKey == null)
+            {
+                throw new ArgumentNullException(nameof(routeKey));
+            }
+
+            if (values == null)
+            {
+                throw new ArgumentNullException(nameof(values));
+            }
+
+            object value;
+            if (values.TryGetValue(routeKey, out value) && value != null)
+            {
+                if (value is DateTime)
+                {
+                    return true;
+                }
+
+                DateTime result;
+                var valueString = Convert.ToString(value, CultureInfo.InvariantCulture);
+
+                if (!string.IsNullOrEmpty(_dateTimeformat))
+                {
+                    var success = DateTime.TryParseExact(valueString, _dateTimeformat, CultureInfo.InvariantCulture, DateTimeStyles.None, out result);
+                    if (success)
+                    {
+                        values[routeKey] = result;
+                    }
+
+                    return success;
+                }
+
+                return DateTime.TryParse(valueString, CultureInfo.InvariantCulture, DateTimeStyles.None, out result);
+            }
+
+            return false;
+        }
+    }
+}
